c1e87d5fe778a8957a6e3aeb81dd71a64e2d30d7
minchan <minchan@minchans-MBP-2.attlocal.net>
2023-06-19 08:34:58 -0500

    added Is Unique

--- Ch01. Arrays and Strings/src/Q01_Is_Unique.java
+++ Ch01. Arrays and Strings/src/Q01_Is_Unique.java
@@ -3,7 +3,13 @@
  * June 19, 2023
  */
 
-public class isUnique {
+public class Q01_Is_Unique {
+    /**
+     * Determines if a string has all unique characters without an
+     * additional data structure (in-place)
+     * @param input String to check for unique characters
+     * @return indication of string containing duplicate characters
+     */
     public static boolean isUnique(String input) {
         // check for duplicates in-place. average O(n^2) implementation
         for (int i = 0; i < input.length(); i++) {
@@ -20,5 +26,9 @@ public class isUnique {
         String test1 = "hello";
         boolean expected1 = false;
         assert expected1 == isUnique(test1);
+
+        String test2 = "no duplicates";
+        boolean expected2 = true;
+        assert expected2 == isUnique(test2);
     }
 }
