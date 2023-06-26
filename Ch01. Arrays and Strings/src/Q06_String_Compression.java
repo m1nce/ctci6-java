27a7b159f3a1ad263943ed220bfc6e659ba01abc
minchan <minchan@dhcp-172-24-168-107.utsarr.net>
2023-06-26 14:30:09 -0500

    completed String Compression

---
+++ Ch01. Arrays and Strings/src/Q06_String_Compression.java
@@ -0,0 +1,52 @@
+/**
+ * Minchan Kim
+ * June 26, 2023
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Q06_String_Compression {
+    public static String stringCompression(String compress) {
+        // initialize instance variables
+        char[] ca = compress.toCharArray();
+        int count = 1;
+        char current_letter = ca[0];
+        StringBuilder newString = new StringBuilder();
+
+        // write the compressed string into the StringBuilder
+        for (int i = 1; i < ca.length; i++) {
+            if (ca[i] != current_letter) {
+                newString.append(current_letter);
+                newString.append(count);
+                count = 0;
+                current_letter = ca[i];
+                count++;
+            } else {
+                count++;
+            }
+
+            // if last letter is reached, automatically add it into the compressed string
+            if (i == ca.length - 1) {
+                newString.append(current_letter);
+                newString.append(count);
+            }
+        }
+
+        // determines if our compressed string is longer than the string we want to compress
+        if (newString.length() >= compress.length()) {
+            return compress;
+        }
+        return newString.toString();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("a2b1c5a3", stringCompression("aabcccccaaa"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("a5b4a4b2d2c1", stringCompression("aaaaabbbbaaaabbddc"));
+    }
+}
